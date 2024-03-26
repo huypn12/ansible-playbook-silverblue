7480c873f58b07105d1ad3d9ab5343a46235f76f
Phung Nhat-Huy <huypn.dev@gmail.com>
2024-03-26 14:59:07 +0100

    bootstrap fix cwd

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 PROVISION_DIR="${HOME}/.provision"
-mkdir ${PROVISION_DIR} && cd ${PROVISION_DIR}
+mkdir ${PROVISION_DIR} 
+pushd ${PROVISION_DIR}
 
-wget -O Miniforge3.sh "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
-bash Miniforge3-$(uname)-$(uname -m).sh -b -p "${PROVISION_DIR}/conda"
+wget "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
+bash ${PROVISION_DIR}/Miniforge3-$(uname)-$(uname -m).sh -b -p "${PROVISION_DIR}/conda"
 source "${PROVISION_DIR}/conda/etc/profile.d/conda.sh"
 conda activate
 conda install -y ansible
 
 wget https://github.com/huypn12/huypn-ansible-laptop/archive/refs/heads/main.zip
 unzip main.zip
+
+popd
